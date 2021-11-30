1a43c3ebe61e082fa3f8edda7e01af6ddb39c44d
Martijn Muijsers <martijnmuijsers@live.nl>
2021-11-30 17:38:19 +0100

    Add default plugin-independent namespace for namespaced keys

---
+++ sucraft-core/src/main/kotlin/org/sucraft/core/common/sucraft/persistentdata/DefaultSuCraftNamespace.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.sucraft.persistentdata
+
+import org.bukkit.NamespacedKey
+
+object DefaultSuCraftNamespace {
+
+	private const val defaultNamespace = "sucraft";
+
+	fun getNamespacedKey(key: String) = NamespacedKey(defaultNamespace, key)
+
+}

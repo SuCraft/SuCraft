b0a172a7f205a3cf1b28575faacaf517f46a180c
Martijn Muijsers <martijnmuijsers@live.nl>
2021-11-30 15:35:43 +0100

    Add closest player finder

---
+++ sucraft-core/src/main/kotlin/org/sucraft/core/common/bukkit/player/ClosestPlayerFinder.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.player
+
+import org.bukkit.Location
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
+import java.util.function.Predicate
+
+object ClosestPlayerFinder {
+
+	fun getClosestPlayer(location: Location, predicate: Predicate<Player>? = null): Player? =
+		location.world.players.minByOrNull{it.location.distanceSquared(location)}
+
+	fun getClosestPlayer(entity: Entity, predicate: Predicate<Player>? = null): Player? =
+		getClosestPlayer(entity.location, predicate)
+
+}

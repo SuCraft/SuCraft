a8a3bf3f9764dec5ae4a76e29722bcf758a565b4
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-01 02:31:52 +0100

    Add white line after imports

--- sucraft-core/src/main/kotlin/org/sucraft/core/common/bukkit/command/CommonTabCompletion.kt
+++ sucraft-core/src/main/kotlin/org/sucraft/core/common/bukkit/command/CommonTabCompletion.kt
@@ -7,6 +7,7 @@ package org.sucraft.core.common.bukkit.command
 import org.sucraft.core.common.sucraft.command.TabCompleter
 import java.util.*
 
+
 object CommonTabCompletion {
 
 	val EMPTY: TabCompleter = { _, _, _, _ -> Collections.emptyList() }

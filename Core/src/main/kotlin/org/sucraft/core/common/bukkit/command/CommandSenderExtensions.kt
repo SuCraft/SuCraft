254ead3ea2e90c02129dec87777f64046ae6e144
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-01 17:57:12 +0100

    Add player visibility extensions

---
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/command/CommandSenderExtensions.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.command
+
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import java.util.*
+
+object CommandSenderExtensions {
+
+	// Visibility (for players)
+
+	fun CommandSender.getVisiblePlayers() =
+		Bukkit.getOnlinePlayers().let {
+			if (this is Player)
+				it.filter { this.canSee(it) }
+			else it
+		}
+
+	fun CommandSender.getHiddenPlayers() =
+		if (this is Player)
+			Bukkit.getOnlinePlayers().filter { !this.canSee(it) }
+		else
+			Collections.emptyList()
+
+}
---
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/player/PlayerExtensions.kt
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.player
+
+
+object PlayerExtensions {}

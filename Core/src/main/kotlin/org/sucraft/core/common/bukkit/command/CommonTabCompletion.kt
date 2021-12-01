2d38e3dda40ec3c9761e441fb1b79379b2da5c10
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-01 20:03:47 +0100

    Add tab completions for online players

--- Core/src/main/kotlin/org/sucraft/core/common/bukkit/command/CommonTabCompletion.kt
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/command/CommonTabCompletion.kt
@@ -4,12 +4,17 @@
 
 package org.sucraft.core.common.bukkit.command
 
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.sucraft.core.common.sucraft.command.TabCompleter
 import java.util.*
+import org.sucraft.core.common.bukkit.command.CommandSenderExtensions.getVisiblePlayers
 
 
 object CommonTabCompletion {
 
 	val EMPTY: TabCompleter = { _, _, _, _ -> Collections.emptyList() }
+	val ONLINE_PLAYERS: TabCompleter = { _, _, _, _ -> Bukkit.getOnlinePlayers().map { it.name } }
+	val VISIBLE_ONLINE_PLAYERS: TabCompleter = { sender, _, _, _ -> sender.getVisiblePlayers().map { it.name } }
 
 }

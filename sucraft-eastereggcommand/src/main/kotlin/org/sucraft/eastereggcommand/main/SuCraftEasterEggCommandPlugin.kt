7df9df4e47905105b72adcecc5952e645fe7a724
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-01 02:29:20 +0100

    Add SuCraft-EasterEggCommand

---
+++ sucraft-eastereggcommand/src/main/kotlin/org/sucraft/eastereggcommand/main/SuCraftEasterEggCommandPlugin.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.eastereggcommand.main
+
+import org.sucraft.core.common.sucraft.plugin.SuCraftPlugin
+import org.sucraft.core.common.general.pattern.SingletonContainer
+import org.sucraft.eastereggcommand.command.SuCraftEasterEggCommandCommands
+
+
+class SuCraftEasterEggCommandPlugin : SuCraftPlugin() {
+
+	// Companion (singleton)
+
+	companion object : SingletonContainer<SuCraftEasterEggCommandPlugin>()
+
+	// Enable
+
+	override fun onSuCraftPluginEnable() {
+		// Initialize components
+		SuCraftEasterEggCommandCommands
+	}
+
+}

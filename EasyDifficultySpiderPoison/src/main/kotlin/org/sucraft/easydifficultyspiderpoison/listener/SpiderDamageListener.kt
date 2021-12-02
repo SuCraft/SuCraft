ac6a8479b5b3b4b86c387ad0116ea369bdcec5cd
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-02 12:37:54 +0100

    Add SuCraft-EasyDifficultySpiderPoison

---
+++ EasyDifficultySpiderPoison/src/main/kotlin/org/sucraft/easydifficultyspiderpoison/listener/SpiderDamageListener.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.easydifficultyspiderpoison.listener
+
+import org.bukkit.Difficulty
+import org.bukkit.entity.CaveSpider
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import org.sucraft.core.common.sucraft.plugin.SuCraftComponent
+import org.sucraft.easydifficultyspiderpoison.main.SuCraftEasyDifficultySpiderPoisonPlugin
+
+
+object SpiderDamageListener : SuCraftComponent<SuCraftEasyDifficultySpiderPoisonPlugin>(SuCraftEasyDifficultySpiderPoisonPlugin.getInstance()) {
+
+	// Settings
+
+	val chanceForPoisonOnDamage = 0.2
+	val poisonDurationInTicks = 20 * 5
+	val poisonAmplifier = 0
+
+	// Events
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
+		if (event.damage <= 0) return
+		if (event.damager !is CaveSpider) return
+		val player = event.entity as? Player ?: return
+		if (player.world.difficulty != Difficulty.EASY) return
+		if (Math.random() >= chanceForPoisonOnDamage) return
+		player.addPotionEffect(PotionEffect(PotionEffectType.POISON, poisonDurationInTicks, poisonAmplifier, false))
+	}
+
+}
---
+++ EasyDifficultySpiderPoison/src/main/kotlin/org/sucraft/easydifficultyspiderpoison/main/SuCraftEasyDifficultySpiderPoisonPlugin.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.easydifficultyspiderpoison.main
+
+import org.sucraft.core.common.sucraft.plugin.SuCraftPlugin
+import org.sucraft.core.common.general.pattern.SingletonContainer
+import org.sucraft.easydifficultyspiderpoison.listener.SpiderDamageListener
+
+
+class SuCraftEasyDifficultySpiderPoisonPlugin : SuCraftPlugin() {
+
+	// Companion (singleton)
+
+	companion object : SingletonContainer<SuCraftEasyDifficultySpiderPoisonPlugin>()
+
+	// Enable
+
+	override fun onSuCraftPluginEnable() {
+		// Initialize components
+		SpiderDamageListener
+	}
+
+}

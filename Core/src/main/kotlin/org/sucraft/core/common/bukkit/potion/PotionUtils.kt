819c3f990c174c4edaca72724e369c8d7ac85ae4
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-02 22:58:29 +0100

    Add potion effect utility

---
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/potion/PotionUtils.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.potion
+
+import org.bukkit.inventory.meta.PotionMeta
+import org.bukkit.potion.PotionType
+
+
+object PotionUtils {
+
+	fun hasNonBasePotionEffect(meta: PotionMeta) =
+		meta.customEffects.isNotEmpty() || isPotionEffectNonBase(meta.basePotionData.type)
+
+	fun isPotionEffectNonBase(type: PotionType) =
+		when (type) {
+			PotionType.WATER, PotionType.AWKWARD, PotionType.MUNDANE, PotionType.THICK -> false
+			else -> true
+		}
+
+}

15a18eb56010364d0285cef22c2ba970dcfa216b
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-03 01:47:19 +0100

    Add SuCraft-VehicleEjectionOnQuit

---
+++ VehicleEjectionOnQuit/src/main/kotlin/org/sucraft/vehicleejectiononquit/main/SuCraftVehicleEjectionOnQuitPlugin.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.vehicleejectiononquit.main
+
+import org.sucraft.core.common.sucraft.plugin.SuCraftPlugin
+import org.sucraft.core.common.general.pattern.SingletonContainer
+import org.sucraft.vehicleejectiononquit.listener.PlayerQuitListener
+
+
+class SuCraftVehicleEjectionOnQuitPlugin : SuCraftPlugin() {
+
+	// Companion (singleton)
+
+	companion object : SingletonContainer<SuCraftVehicleEjectionOnQuitPlugin>()
+
+	// Enable
+
+	override fun onSuCraftPluginEnable() {
+		// Initialize components
+		PlayerQuitListener
+	}
+
+}

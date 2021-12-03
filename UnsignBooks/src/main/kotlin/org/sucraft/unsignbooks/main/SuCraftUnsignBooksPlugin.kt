56d0b5016704e9b1fa003561bde82a0ff8b2e5e9
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-03 02:09:29 +0100

    Add SuCraft-UnsignBooks

---
+++ UnsignBooks/src/main/kotlin/org/sucraft/unsignbooks/main/SuCraftUnsignBooksPlugin.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.unsignbooks.main
+
+import org.sucraft.core.common.sucraft.plugin.SuCraftPlugin
+import org.sucraft.core.common.general.pattern.SingletonContainer
+import org.sucraft.unsignbooks.command.SuCraftUnsignBooksCommands
+
+
+class SuCraftUnsignBooksPlugin : SuCraftPlugin() {
+
+	// Companion (singleton)
+
+	companion object : SingletonContainer<SuCraftUnsignBooksPlugin>()
+
+	// Enable
+
+	override fun onSuCraftPluginEnable() {
+		// Initialize components
+		SuCraftUnsignBooksCommands
+	}
+
+}
---
+++ UnsignBooks/src/main/kotlin/org/sucraft/unsignbooks/player/permission/SuCraftUnsignBooksPermissions.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.unsignbooks.player.permission
+
+import org.sucraft.core.common.sucraft.player.permission.SuCraftPermissions
+
+
+object SuCraftUnsignBooksPermissions : SuCraftPermissions("unsignbooks") {
+
+	val UNSIGN_OWN_BOOKS = createPermission("unsign.own")
+	val UNSIGN_OTHER_PLAYERS_BOOKS = createPermission("unsign.other")
+
+}

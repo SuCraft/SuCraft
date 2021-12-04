8147ecd43b5a00ad0b46c77a2eeb4cd2b715bf28
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-04 23:17:50 +0100

    Add entity age utility

---
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/entity/EntityAge.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.entity
+
+import org.bukkit.entity.Ageable
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Zombie
+
+
+object EntityAge {
+
+	fun isAdult(entity: LivingEntity): Boolean? {
+		if (entity is Ageable) return entity.isAdult
+		if (entity is Zombie) return entity.isAdult
+		return false
+	}
+
+	fun isBaby(entity: LivingEntity) = isAdult(entity)?.let { !it }
+
+	fun setAdult(entity: LivingEntity) {
+		if (entity is Ageable) {
+			entity.setAdult()
+			return
+		}
+		if (entity is Zombie) {
+			entity.setAdult()
+			return
+		}
+	}
+
+	fun setBaby(entity: LivingEntity) {
+		if (entity is Ageable) {
+			entity.setBaby()
+			return
+		}
+		if (entity is Zombie) {
+			entity.setBaby()
+			return
+		}
+	}
+
+}

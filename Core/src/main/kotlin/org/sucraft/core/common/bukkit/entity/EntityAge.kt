b089b635cd1df2787fb283971b368958f794b7c7
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-29 14:34:21 +0100

    Remove Zombie check, because the Zombie interface now extends the Ageable interface

--- Core/src/main/kotlin/org/sucraft/core/common/bukkit/entity/EntityAge.kt
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/entity/EntityAge.kt
@@ -6,14 +6,12 @@ package org.sucraft.core.common.bukkit.entity
 
 import org.bukkit.entity.Ageable
 import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Zombie
 
 
 object EntityAge {
 
 	fun isAdult(entity: LivingEntity): Boolean? {
 		if (entity is Ageable) return entity.isAdult
-		if (entity is Zombie) return entity.isAdult
 		return null
 	}
 
@@ -24,18 +22,10 @@ object EntityAge {
 			entity.setAdult()
 			return
 		}
-		if (entity is Zombie) {
-			entity.setAdult()
-			return
-		}
 	}
 
 	fun setBaby(entity: LivingEntity) {
 		if (entity is Ageable) {
-			entity.setBaby()
-			return
-		}
-		if (entity is Zombie) {
 			entity.setBaby()
 			return
 		}

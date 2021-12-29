8c352e02b275cf7fa85345c5555c01d6a072dd36
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-29 13:16:41 +0100

    Create plugin folders utility class

---
+++ Core/src/main/kotlin/org/sucraft/core/common/bukkit/io/PluginFolders.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.bukkit.io
+
+import java.io.File
+import java.nio.file.Path
+
+
+object PluginFolders {
+
+	private const val pluginsFolderName = """plugins"""
+
+	fun getPluginsFolder() = File(pluginsFolderName)
+
+	fun getPluginDataFolder(pluginName: String) = Path.of(getPluginsFolder().path, pluginName).toFile()
+
+}

c15f7c10baebcb83d40e1b1ca81a3455ae38d848
Martijn Muijsers <martijnmuijsers@live.nl>
2021-12-30 15:41:13 +0100

    Add short-term TPS measurement delegate

---
+++ Core/src/main/kotlin/org/sucraft/core/common/sucraft/delegate/measuretps/ShortTermMeasureTPS.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) SuCraft 2021 sucraft.org
+ */
+
+package org.sucraft.core.common.sucraft.delegate.measuretps
+
+import org.sucraft.core.common.sucraft.plugin.SuCraftDelegate
+import org.sucraft.core.common.sucraft.plugin.SuCraftDelegateHolder
+import org.sucraft.core.common.sucraft.plugin.SuCraftPlugin
+
+
+interface ShortTermMeasureTPS<P: SuCraftPlugin> : SuCraftDelegate<P> {
+
+	// Companion (implementation)
+
+	companion object : SuCraftDelegateHolder<ShortTermMeasureTPS<*>>()
+
+	// Delegate name
+
+	override fun getDelegateInterfaceName() = ShortTermMeasureTPS::class.simpleName!!
+
+	// Interface
+
+	fun getRecentTPS(): Double
+
+	fun registerListener(listener: ShortTermMeasuredTPSListener)
+
+}
